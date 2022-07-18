e6c2c53f951e4a819da1619fec601faec9f9461a
Kang <khighbig@gmail.com>
2022-07-18 17:57:26 +0900

    메타테이블 컬럼항목 작업중

--- src/main/java/kr/or/lx/dms/metaTable/controller/MetaTableController.java
+++ src/main/java/kr/or/lx/dms/metaTable/controller/MetaTableController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,9 +42,10 @@ public class MetaTableController {
 		return "dms/metaTable/metaInfo";
 	}
 	
-	@GetMapping("/columnInfo")
-	public String columnInfo(ModelMap model) throws Exception{
+	@GetMapping("/columnInfo/{tableIdntfcId}")
+	public String columnInfo(@PathVariable String tableIdntfcId, ModelMap model) throws Exception{
 		
+		model.addAttribute("table_idntfc_id", tableIdntfcId);
 		
 		return "dms/metaTable/columnInfo";
 	}
