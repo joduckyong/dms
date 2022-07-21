d02c122ab2edb98f8cd863c02a90fd2965b57f09
LAPTOP-AHDO2SSJ\kang <khighbig@gmai.com>
2022-07-21 21:46:35 +0900

    수집오류 상세화면

--- src/main/java/kr/or/lx/dms/collectionError/controller/CollectionErrorController.java
+++ src/main/java/kr/or/lx/dms/collectionError/controller/CollectionErrorController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,9 +35,10 @@ public class CollectionErrorController {
 		return "dms/collectionError/list";
 	}	
 	
-	@GetMapping("/detail")
-	public String detail(ModelMap model) throws Exception{
+	@GetMapping("/detail/{instanceId}")
+	public String detail(@PathVariable String instanceId, ModelMap model) throws Exception{
 		
+		model.addAttribute("instance_id", instanceId);
 		
 		return "dms/collectionError/detail";
 	}	
